764a4d9247e9b138b53934a1704e7bf9a92ef61f
stharej1 <solankythareja@gmail.com>
2016-09-14 17:30:55 -0400

    Initial Commit

---
+++ COMP229-F2016-Lesson1/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Landing Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" 
+    CodeBehind="Default.aspx.cs" Inherits="COMP229_F2016_Lesson1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1><asp:Label ID="HelloLabel" runat="server" Text="Hello World!"></asp:Label></h1>
+    <p>This is my fist Web App using ASP.NET Web Forms</p>
+    
+
+</asp:Content>
---
+++ COMP229-F2016-Lesson1/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP229_F2016_Lesson1
+{
+    public partial class _Default : Page
+    {
+        /**
+         * <summary>This method executes every time the page loads</summary>
+         */
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "Good Bye!";
+        }
+    }
+}
